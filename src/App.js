80a94fa503c7243db4ecf9a106f0e5f029cb2380
Shruti Salunkhe <shrutisubhash16@gmail.com>
2022-01-23 19:12:26 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  //Use state is a statefull variable which can use a data
+  const [users, setUser] = useState([]);
+  const loadUsers = async () => {
+    console.log("before");
+    const response = await fetch("https://reqres.in/api/users?page=1");
+    const jsonResponse = await response.json();
+    console.log(jsonResponse.data);
+    setUser(jsonResponse.data);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>LGMVIP - TASK 2</h1>
+      <h4>Users : </h4>
+      <button onClick={loadUsers}>Get Data</button>
+
+      <ul>
+        {users.map(({ id, email, first_name,last_name,avatar }) => (
+          <li key={id}>
+            <div className="image"><img src={avatar}/></div>
+            <h2>{first_name} {last_name}</h2>
+            <h3>{email}</h3>
+           
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
